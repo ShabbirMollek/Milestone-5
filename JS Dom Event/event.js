b0f2c5c5cedf6cc2b8fc2cac797e8ac1ec4d354b
Sabbirador <sabbirador69@gmail.com>
2024-02-12 00:13:58 +0600

    clicked to button changed text or any other things

---
+++ JS Dom Event/event.js
@@ -0,0 +1,10 @@
+
+function makeRed(){
+                document.body.style.backgroundColor='red';
+}
+
+// most used
+document.getElementById('orange').addEventListener('click', function(){
+        document.body.style.backgroundColor='orange';
+}
+)
