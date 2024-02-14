cac4295025a6986c435aff0f4371404b02d9e1a5
Sabbirador <sabbirador69@gmail.com>
2024-02-14 14:52:56 +0600

    geometry genius

--- JS Dom Event/event.js
+++ JS Dom Event/event.js
@@ -8,3 +8,21 @@ document.getElementById('orange').addEventListener('click', function(){
         document.body.style.backgroundColor='orange';
 }
 )
+
+
+
+function doSomethingAsync(callback) {
+        // Simulating an asynchronous operation, like fetching data from an API
+        setTimeout(function() {
+          console.log("Async operation completed");
+          callback(); // Call the callback function once the asynchronous operation is done
+        }, 1000);
+      }
+      
+      function handleCallback() {
+        console.log("Callback function executed");
+      }
+      
+      // Call the function with a callback
+      doSomethingAsync(handleCallback);
+      
